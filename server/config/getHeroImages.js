7b2005f2c442ee84a17ac80915e51ba1f80801aa
Joseph Pasque <pasquej@wustl.edu>
2017-07-16 11:19:58 -0500

    adding image config file

--- server/config/getHeroImages.js
+++ server/config/getHeroImages.js
@@ -1,5 +1,36 @@
-const request = require("request");
 
+
+var http = require('http')
+  , fs = require('fs')
+
+var imgUrl = "http://cdn.dota2.com/apps/dota2/images/heroes/"
 var heroList = JSON.parse("../static/heroList.json");
 
 
+buildRequest = (heroName, suffix, queryString) => {
+    var request = http.get(imgUrl + queryString, function(res){
+        var imagedata = ''
+        res.setEncoding('binary')
+
+        res.on('data', function(chunk){
+            imagedata += chunk
+        })
+
+        res.on('end', function(){
+            fs.writeFile('../static/imgs/' + queryString, imagedata, 'binary', function(err){
+                if (err) throw err
+                console.log('File saved.')
+            })
+        })
+
+    })
+}
+
+
+for (var i=0; i< heroList.length; i++) {
+    var hero = heroList[i];
+    var heroName = hero.name.replace('npc_dota_hero','');
+    var suff = "sb.png"
+    queryString = heroName + "_" + suff;
+    buildRequest(heroName, suffix, queryString);
+}
