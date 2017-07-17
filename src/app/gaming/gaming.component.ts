3c40938a2cc39e5dce225653612d3b033d76df1a
PASQUE <jp606j@att.com>
2017-07-17 11:06:51 -0500

    built basic html/js for loading hero image

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -9,16 +9,28 @@ import { DotaGame } from './dota-game';
 })
 export class GamingComponent implements OnInit {
   mostRecentGame: any;
+  gameLoaded: boolean;
+  heroImageLoaded: boolean;
+  heroImageURI: String;
 
   constructor(private gamingService: GamingService) { 
     this.mostRecentGame = {};
+    this.gameLoaded = false;
+    this.heroImageLoaded = false;
+    this.heroImageURI = "";
   }
 
   ngOnInit() {
     this.gamingService.getMostRecentDotaGame().subscribe((resp) =>{
       this.mostRecentGame = resp;
+      this.gameLoaded = true;
+      this.buildHeroImageURI();
     },(error) => {
     })
+  }
+
+  buildHeroImageURI() {
+    //build img string
   }
 
 }
