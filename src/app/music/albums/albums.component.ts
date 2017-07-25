ee24ad22f32eeffc79c2a958e6dfe90330b2f71d
Joseph Pasque <pasquej@wustl.edu>
2017-07-25 18:22:35 -0500

    album images

--- src/app/music/albums/albums.component.ts
+++ src/app/music/albums/albums.component.ts
@@ -35,8 +35,8 @@ import {
       state('clicked',style({
         transform: 'scale(3)',
         'transform-origin': '0 0',
-        top: '25px',
-        left: '150px',
+        bottom: '25px',
+        right: '150px',
         opacity: 1,
         'z-index': 2,
         'box-shadow': '0 0 20px 10px rgba(0,0,0,0.4)'
