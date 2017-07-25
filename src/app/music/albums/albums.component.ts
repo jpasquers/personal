1f6499feffefa266fb8361ed147d7349cd53819e
Joseph Pasque <pasquej@wustl.edu>
2017-07-25 17:51:39 -0500

    album images

--- src/app/music/albums/albums.component.ts
+++ src/app/music/albums/albums.component.ts
@@ -1,25 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import {AlbumsService} from './albums.service';
+import {Observable} from 'rxjs/Rx';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
-  styleUrls: ['./albums.component.css']
+  styleUrls: ['./albums.component.css'],
+  animations: [
+    trigger('imgState', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('visible', style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      state('hover', style({
+        transform: 'scale(1.1)',
+      })),
+      state('clicked',style({
+        transform: 'scale(3)'
+      })),
+      transition('* => *', animate('100ms ease-in'))
+    ])
+  ]
 })
 export class AlbumsComponent implements OnInit {
   savedAlbums: any[];
+  imgsLoaded: number;
   Math: any;
   constructor(private albumsService: AlbumsService) {
     this.savedAlbums = [];
     this.Math = Math;
+    this.imgsLoaded = 0;
    }
 
   ngOnInit() {
     this.albumsService.getSavedAlbums().subscribe((albums) => {
       console.log(albums);
-      this.savedAlbums = albums;
+      this.savedAlbums = albums.map((album) => {
+        album.state="hidden";
+        return album;
+      });
     }, (err) => {
       console.log(err);
+    })
+  }
+
+  imgLoaded() {
+    if (++this.imgsLoaded == this.savedAlbums.length) this.displayImgs();
+  }
+
+  displayImgs() {
+    var obs = Observable.timer(0,150)
+      .take(this.imgsLoaded);
+    var observe = obs.subscribe((i) => {
+      this.savedAlbums[i].state = "visible"
     })
   }
 
--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { MusicRoutingModule } from './music-routing.module';
@@ -25,6 +26,7 @@ import { AlbumsService } from './albums/albums.service';
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     MusicRoutingModule,
     VertNavModule
   ],
